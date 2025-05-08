24dcf4e6160accdef7158dc6dcf84a666edb6854
Kasper Nielsen <kasper.nielsen@lego.com>
2025-05-08 13:53:49 +0200

    initial commit

--- backend/settings.py
+++ backend/settings.py
@@ -44,8 +44,8 @@ class _UiSettings(BaseSettings):
     title: str = "Contoso"
     logo: Optional[str] = None
     chat_logo: Optional[str] = None
-    chat_title: str = "Start chatting"
-    chat_description: str = "This chatbot is configured to answer your questions"
+    chat_title: str = "Input device ID to be investigated!"
+    chat_description: str = "This AI engine helps Poly engineers troubleshoot device health"
     favicon: str = "/favicon.ico"
     show_share_button: bool = True
     show_chat_history_button: bool = True
